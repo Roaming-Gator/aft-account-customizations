ca691fd135889f2b72f33751675efe5cef3a77c5
Paul Nickerson <pvnick>
2023-04-17 08:44:55 -0400

    enable terraform cloud access

--- slackgpt/terraform/role.tf
+++ slackgpt/terraform/role.tf
@@ -1,10 +1,10 @@
-# create an iam role for github actions to assume
+# enable terraform cloud to assume a role in the slackgpt accounts and perform administrative actions
 
 # get current account id
 data "aws_caller_identity" "current" {}
 
-# allow the specified github repo to assume the role
-resource "aws_iam_role" "github_actions" {
+# allow the specified terraform cloud project to assume the role
+resource "aws_iam_role" "terraform_cloud" {
   name = "github-actions"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -15,13 +15,13 @@ resource "aws_iam_role" "github_actions" {
           {
             "Effect" : "Allow",
             "Principal" : {
-              "Federated" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+              "Federated" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/app.terraform.io"
             },
             "Action" : "sts:AssumeRoleWithWebIdentity",
             "Condition" : {
               "StringEquals" : {
-                "token.actions.githubusercontent.com:sub" : "repo:Roaming-Gator/slackgpt:ref:refs/heads/*",
-                "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+                "app.terraform.io:sub" : "organization:roaminggator:project:slackgpt:workspace:*:run_phase:*",
+                "app.terraform.io:aud" : "aws.workload.identity"
               }
             }
           }
@@ -31,24 +31,24 @@ resource "aws_iam_role" "github_actions" {
   })
 }
 
-# associate the needed policies with the github actions role 
+# allow the assumed role to perform the needed actions 
 
 resource "aws_iam_role_policy_attachment" "lambda-full-access" {
-  role       = aws_iam_role.github_actions.name
+  role       = aws_iam_role.terraform_cloud.name
   policy_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "dynamodb-full-access" {
-  role       = aws_iam_role.github_actions.name
+  role       = aws_iam_role.terraform_cloud.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "secretsmanager-read-write" {
-  role       = aws_iam_role.github_actions.name
+  role       = aws_iam_role.terraform_cloud.name
   policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
 
 resource "aws_iam_role_policy_attachment" "iam-full-access" {
-  role       = aws_iam_role.github_actions.name
+  role       = aws_iam_role.terraform_cloud.name
   policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
 }
