20dc8ffc7a31520c9d0432c7063d61c1e6ee1231
Paul Nickerson <pvnick>
2023-05-05 21:34:44 -0400

    resource sharing

--- networking/terraform/main.tf
+++ networking/terraform/main.tf
@@ -1,6 +1,12 @@
 module "basic" {
   source = "github.com/aws-ia/terraform-aws-ipam"
 
-  top_cidr = ["10.0.0.0/8"]
-  top_name = "basic ipam"
+  top_cidr        = ["10.0.0.0/8"]
+  top_name        = "Internal IP Space"
+  ipam_scope_type = "private"
+  ram_share_principals = [
+    data.aws_organizations_organization.org.id
+  ]
 }
+
+data "aws_organizations_organization" "org" {}
